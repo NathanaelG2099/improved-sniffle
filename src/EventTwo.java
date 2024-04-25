a05cf19f7eba503c34b4e1c0d7d05e120d263173
Nathanael Garcia <nathanael.garcia@sjsu.edu>
2024-04-25 14:31:08 -0700

    Push #9 Furthered EventOne

---
+++ src/EventTwo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EventTwo {
+    public static void Start(Scanner scnr, Day day, EventRecord timeline){
+
+    }
+
+    public static void Church(Scanner scnr, Day day, EventRecord timeline){
+
+    }
+
+    public static void Town(Scanner scnr, Day day, EventRecord timeline){
+
+    }
+
+    public static void TownMagnus(Scanner scnr, Day day, EventRecord timeline){
+
+    }
+
+    public static void TownAlone(Scanner scnr, Day day, EventRecord timeline){
+
+    }
+}
